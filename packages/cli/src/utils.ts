625f589d18f7811b5c10a2b62e3631d59a839caa
Ming-der Wang <mingderwang@gmail.com>
2023-08-07 18:36:46 +0800

    verbose with correct name

--- packages/cli/src/utils.ts
+++ packages/cli/src/utils.ts
@@ -1,6 +1,6 @@
 import Debug from 'debug';  
   
-// TODO replace `obsidian-album` with a friendly short label that describe best your library  
+// TODO replace `@muzamint/cli 🔬🏃‍♀️` with a friendly short label that describe best your library  
 export const rootDebug = Debug('obsidian-album')  
   
 export const printVerboseHook = (thisCommand) => {  
@@ -8,7 +8,7 @@ export const printVerboseHook = (thisCommand) => {
   const options = thisCommand.opts();  
   
   if (options.verbose) {  
-    Debug.enable('obsidian-album*');  
+    Debug.enable('@muzamint/cli 🔬🏃‍♀️*');  
     rootDebug(`CLI arguments`);  
     rootDebug(options);  
   }  
